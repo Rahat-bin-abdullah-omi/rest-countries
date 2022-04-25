22f24abb79f74700ec416a08ab0e6a44543f13e7
Rahat Bin Abdullah Omi <rahatomi123@gmail.com>
2022-04-25 13:17:15 +0600

    Footer added

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p><small>Copyright © hercules</small></p>
+        </div>
+    );
+};
+
+export default Footer;
